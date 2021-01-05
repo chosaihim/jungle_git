651709ab7ba400ba5b2d7da5e75a9c0bf38bbc0b
chosaihim <chosaihim@gmail.com>
2021-01-05 15:25:26 +0900

    minor edits

--- WEEK4/6_2098_외판원순회.py
+++ WEEK4/6_2098_외판원순회.py
@@ -7,19 +7,19 @@ costs = [list(map(int, input().split())) for i in range(n)]
 cache = [[0]*(1<<n) for _ in range(n)]
 
 
-def travel(last, visited):
+def travel(start, visited):
     
-    if visited == ((1<<n)-1): #return costs[last][0] or INF
-        if costs[last][0] != 0 :return costs[last][0]
+    if visited == ((1<<n)-1):
+        if costs[start][0] != 0 :return costs[start][0]
         else: return 10**10
-    if cache[last][visited]: return cache[last][visited]
+    if cache[start][visited]: return cache[start][visited]
     
     temp = 10**10
     for city in range(n):
-        if visited & (1<<city) == 0 and costs[last][city] != 0:
-            temp = min(temp, travel(city, visited | (1<<city))+costs[last][city])
+        if visited & (1<<city) == 0 and costs[start][city] != 0:
+            temp = min(temp, travel(city, visited | (1<<city))+costs[start][city])
     
-    cache[last][visited] = temp
+    cache[start][visited] = temp
     return temp
 
 print(travel(0, 1<<0))
