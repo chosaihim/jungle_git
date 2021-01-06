40110c97e662b4d361f28b14eb6d6700c7d30b96
chosaihim <chosaihim@gmail.com>
2021-01-06 15:34:50 +0900

    shorter code

--- WEEK4/DP_practice/1463_1로만들기.py
+++ WEEK4/DP_practice/1463_1로만들기.py
@@ -6,13 +6,7 @@ n = int(input())
 dp = [1] * (n+1)
 dp[1] = 0
 
-for i in range(1, n+1):
-
-    calc1 = calc2 = calc3 = 10**10
-    if not i % 3: calc1 = dp[i//3]
-    if not i % 2: calc2 = dp[i//2]
-    calc3 = dp[i-1]
-
-    dp[i] = 1 + min(calc1, calc2, calc3)
+for i in range(4, n+1):
+    dp[i] = 1+ min(dp[i//3]+i%3, dp[i//2]+i%2)
 
 print(dp[n])
