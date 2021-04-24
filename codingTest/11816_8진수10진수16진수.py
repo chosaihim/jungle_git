4ddf3234deaf7f91084292413f809a562f4c5fec
chosaihim <chosaihim@gmail.com>
2021-04-24 14:25:15 +0900

    better functional programming 11816

--- codingTest/11816_8진수10진수16진수.py
+++ codingTest/11816_8진수10진수16진수.py
@@ -3,36 +3,50 @@
 import sys
 x = input()
 
-def x_num(x):
-    ret = 0
+# def x_num(x):
+#     ret = 0
     
-    if x[0] != '0':
-        return x      
-    else:
-        if len(x) == 1:
-            return x
-        else:
-            if x[1] == 'x':
-                power = 1
-                for i in range(len(x)-1,1,-1):
-                    # print(f'i:{i}, x[{i}]={x[i]}')
-                    if(ord(x[i])>96):
-                        # print(f'{i} alphabet: {x[i]}')
-                        ret += (ord(x[i])-87) * power
-                    else:
-                        # print(f'{i} digit: {x[i]}')
-                        ret += int(x[i]) * power
-                    power *= 16
+#     if x[0] != '0':
+#         return x      
+#     else:
+#         if len(x) == 1:
+#             return x
+#         else:
+#             if x[1] == 'x':
+#                 power = 1
+#                 for i in range(len(x)-1,1,-1):
+#                     # print(f'i:{i}, x[{i}]={x[i]}')
+#                     if(ord(x[i])>96):
+#                         # print(f'{i} alphabet: {x[i]}')
+#                         ret += (ord(x[i])-87) * power
+#                     else:
+#                         # print(f'{i} digit: {x[i]}')
+#                         ret += int(x[i]) * power
+#                     power *= 16
                         
-            else:
-                power = 1
-                for i in range(len(x)-1,-1,-1):
-                    ret += int(x[i]) * power
-                    power *= 8
+#             else:
+#                 power = 1
+#                 for i in range(len(x)-1,-1,-1):
+#                     ret += int(x[i]) * power
+#                     power *= 8
                     
-    return ret
+#     return ret
 
-print(x_num(x))
+def octToDec(x):
+    return int(x[1:],8)
+def hexToDec(x):
+    return int(x[2:],16)
+
+def convert(x):
+    if(x[0:2] == "0x"):
+        return hexToDec(x)
+    elif(x[0] == "0"):
+        return octToDec(x)
+    else:
+        return x
+    
+
+print(convert(x))
 
 ### 정답!!
 ## t=input();c=[[0,1][t[0]=='0'],2][t[1]=='x'];print(int(t[c:],[10,8,16][c]))
