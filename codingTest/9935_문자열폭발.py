b37a4296c7a5afa114b4a4c8bf8a3a9d2599ab97
chosaihim <chosaihim@gmail.com>
2021-04-27 16:57:04 +0900

    문자열 폭발

---
+++ codingTest/9935_문자열폭발.py
@@ -0,0 +1,41 @@
+# https://www.acmicpc.net/problem/9935
+
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+#input from 
+input_str = list(input().strip())
+bomb = input().strip()
+
+bomb_len = len(bomb)
+answer = deque()
+
+
+def compareString(answer, bomb):
+    for i in range(len(bomb)):
+        if answer[len(answer)-(len(bomb))+i] != bomb[i]:
+            return False
+    return True       
+    
+
+for next_char in input_str:
+    answer.append(next_char)
+    if next_char == bomb[-1] and len(answer) >= bomb_len:
+        if compareString(answer, bomb):
+            for i in range(bomb_len):
+                answer.pop()
+    
+
+# print("answer: ", answer)
+
+print("".join(answer)) if answer else print("FRULA")
+
+
+# 시간초과
+# in_str = in_str.replace(bomb,"")
+# prev_str_len = 0
+# while (prev_str_len != len(in_str)):
+#     prev_str_len = len(in_str)
+#     in_str = in_str.replace(bomb,"")
+# print(in_str) if len(in_str)>0 else print("FRULA")
