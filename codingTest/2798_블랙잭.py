a8315789300f784abd78b5411c2a2c9ac13359e8
chosaihim <chosaihim@gmail.com>
2021-04-28 01:25:53 +0900

    combination 보다 삼중 for 문이 더 빠르다.

--- codingTest/2798_블랙잭.py
+++ codingTest/2798_블랙잭.py
@@ -17,4 +17,22 @@ for i in range(len(card_combination)):
     
         
 print(answer)
+
+
+## solution
+# def GetMaxBlackJackNumbers(N, M, nums):
+#     	sums = set()
+# 	for i in range(N-2):
+# 		for j in range(i+1, N-1):
+# 			for k in range(j+1, N):
+# 				sum = nums[i] + nums[j] + nums[k]
+# 				if sum <= M:
+# 					sums.add(sum)
+# 					break
+
+# 	return max([*sums])
+
+# N, M = map(int, input().split())
+# nums = sorted(list(map(int, input().split())), reverse=True)
+# print(GetMaxBlackJackNumbers(N, M, nums))
     
