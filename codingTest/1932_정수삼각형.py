470c5d5d0b76605effbbdafa9221802631b8146e
chosaihim <chosaihim@gmail.com>
2021-05-01 16:36:14 +0900

    1932 정수삼각형. DP 난이도 하

---
+++ codingTest/1932_정수삼각형.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+triangle = [list(map(int,input().split())) for _ in range(n)]
+
+
+def max_sum(triangle):
+    
+    for i in range(1,len(triangle)):
+        for j in range(i+1):
+            if j == 0: triangle[i][j] += triangle[i-1][j]
+            elif j == i: triangle[i][j] += triangle[i-1][j-1]
+            else: triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+        
+        # print(triangle)
+        
+    return max(triangle[len(triangle)-1])
+        
+print(max_sum(triangle))
