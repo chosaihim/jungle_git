4c769f02e3e768b827ce105017170a5a8af407b2
chosaihim <chosaihim@gmail.com>
2021-05-06 22:14:02 +0900

    정렬된 배열에서 특정 수의 개수 구하기 책 예제, 답없음. 시간복잡도 log(n) 이하로 떨어뜨리는 것이 조건

---
+++ codingTest/정렬된배열에서특정수의개수구하기.py
@@ -0,0 +1,62 @@
+import sys
+input = sys.stdin.readline
+
+n, key = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+
+def low_bound(a, key):
+    pl = 0          # 검색 범위의 맨 앞 원소 인덱스
+    pr = len(a) - 1 # 검색 범위의 맨 끝 원소의 인덱스
+
+    while True:
+        pc = (pl+pr)//2     #중앙 원소의 인덱스
+
+        if a[pc] == key and a[pc-1] < key:    
+            # return pc       #검색 성공
+            return pc
+        elif a[pc] < key:   
+            pl = pc + 1     # 검색 범위를 뒤쪽 절반으로
+        else:               
+            pr = pc -1      # 검색 범위를 앞쪽 절반으로
+        
+        if pl > pr:
+            break
+
+        # return -1   # 검색실패
+    return -1
+
+
+def up_bound(a, key):
+    pl = 0          # 검색 범위의 맨 앞 원소 인덱스
+    pr = len(a) - 1 # 검색 범위의 맨 끝 원소의 인덱스
+
+    while True:
+        pc = (pl+pr)//2     #중앙 원소의 인덱스
+
+        if a[pc] == key and a[pc+1] > key:    
+            # return pc       #검색 성공
+            return pc
+        elif a[pc] <= key:   
+            pl = pc + 1     # 검색 범위를 뒤쪽 절반으로
+        else:               
+            pr = pc -1      # 검색 범위를 앞쪽 절반으로
+        
+        if pl > pr:
+            break
+
+        # return -1   # 검색실패
+    return -1
+
+upper = up_bound(numbers, key)
+if upper == -1: print(-1)
+else:
+    print(upper - low_bound(numbers, key) + 1)
+
+# 7 2
+# 1 1 2 2 2 2 3
+# -> 4
+
+# 7 4
+# 1 1 2 2 2 2 3
+# -> -1
