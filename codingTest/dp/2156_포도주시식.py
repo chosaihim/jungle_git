056f5c984196ef6397d860e18773811104a515e2
chosaihim <chosaihim@gmail.com>
2021-05-07 12:42:00 +0900

    포도주시식 error fixed

--- codingTest/dp/2156_포도주시식.py
+++ codingTest/dp/2156_포도주시식.py
@@ -6,10 +6,11 @@ wine = [int(input()) for _ in range(n)]
 dp = [0] * n
 
 dp[0] = wine[0]
-dp[1] = wine[0] + wine[1]
-dp[2] = max(wine[0] + wine[1], wine[0] + wine[2], wine[1] + wine[2])
-
-for i in range(3, n):
-    dp[i] = max(dp[i-1], dp[i-2] + wine[i], dp[i-3] + wine[i-1] + wine[i])
+if n > 1: dp[1] = wine[0] + wine[1]
+if n > 2: 
+    dp[2] = max(wine[0] + wine[1], wine[0] + wine[2], wine[1] + wine[2])
+    
+    for i in range(3, n):
+        dp[i] = max(dp[i-1], dp[i-2] + wine[i], dp[i-3] + wine[i-1] + wine[i])
 
 print(dp[n-1])
