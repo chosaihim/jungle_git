3a7ac41173758c7330f7a702e93f3b1d4aa2bcfd
chosaihim <chosaihim@gmail.com>
2021-05-15 13:33:20 +0900

    크로아티아 알파벳

---
+++ codingTest/basic/2941_크로아티아알파벳.py
@@ -0,0 +1,18 @@
+#
+# import re
+
+# st = input()
+
+# alphabets = re.findall('c=|c-|dz=|d-|lj|nj|s=|z=',st)
+# st = re.sub('c=|c-|dz=|d-|lj|nj|s=|z=','!',st)
+# alphabets += re.findall('[a-z]',st)
+
+# print(len(alphabets))
+
+# 52ms
+# 이해하기
+# c = input().count;
+# print(c)
+# print(c('')-1-sum(map(c,['-','=','nj','lj','dz='])))
+
+
