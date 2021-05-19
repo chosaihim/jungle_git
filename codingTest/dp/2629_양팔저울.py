165cfab2b96c3979b217a7218957831534e9003b
chosaihim <chosaihim@gmail.com>
2021-05-19 22:38:19 +0900

    양팔저울 푸는 중

---
+++ codingTest/dp/2629_양팔저울.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+number_of_weights = int(input())
+weights = list(map(int,input().split()))
+number_of_beads = int(input())
+beads = list(map(int,input().split()))
+
