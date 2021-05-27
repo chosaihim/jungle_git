c9fbaf6305f3cc6163bdca4de9b9ade3130e7d10
chosaihim <chosaihim@gmail.com>
2021-05-27 11:03:33 +0900

    영훈이의 색칠공부 시간초과

--- codingTest/dp/14578_영훈이의색칠공부.py
+++ codingTest/dp/14578_영훈이의색칠공부.py
@@ -1,7 +1,6 @@
 import sys
 input = sys.stdin.readline
-
-# 1,000,000,007
+sys.setrecursionlimit(10**6)
 
 n = int(input())
 
@@ -9,34 +8,40 @@ red = [False] * n
 blue = [False] * n
 count = [0]
 
-def printRow(red, blue, n):
+board = [['']* n for _ in range(n)]
+
+def printBoard():
     for i in range(n):
-        if i == red:
-            print("r", end = '')
-        elif i == blue:
-            print("b", end = '')
-        else:
-            print("x", end = '')
-    print()
+        for j in range(n):
+            if board[i][j] == 'r':
+                print("r", end = '')
+            elif board[i][j] == 'b':
+                print("b", end = '')
+            else:
+                print("□", end = '')
+        print()
+
 
 def color(n, row):
-    print(row)
     for r_col in range(n):
         if not red[r_col]:
-            red[r_col] = True
             for b_col in range(n):
                 if b_col != r_col:
                     if not blue[b_col]:
+                        # board[row][r_col] = 'r'
+                        # board[row][b_col] = 'b'
+                        red[r_col] = True
                         blue[b_col] = True
-                        printRow(r_col, b_col, n)
                         if row == n-1:
-                            print("--------")
+                            # print("--------")
+                            # printBoard()
                             count[0] += 1
                         else:
                             color(n,row+1)
-                            blue[b_col] = False
-            red[r_col] = False
-            
-
+                        # board[row][r_col] = ''
+                        # board[row][b_col] = ''
+                        blue[b_col] = False
+                        red[r_col] = False
+        
 color(n,0)
-print(count[0])
+print(count[0]%1000000007)
