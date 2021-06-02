e96e18c0278e7e7c86b682e6e132ed30deb031c0
chosaihim <chosaihim@gmail.com>
2021-06-02 22:51:09 +0900

    1149 RGB거리 완성안됨.

---
+++ codingTest/graph/1149_RGB거리.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = []
+min_cost = 1000001
+
+def dfs(cost)
+    global min_cost
+    
+    
+        
+    
+    
+
+for i in range(n):
+    r,g,b = map(int,input().split())
+    graph.append([r,g,b])
+    
+
+    
+    
