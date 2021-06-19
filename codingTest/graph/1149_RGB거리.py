c0c51dd1c03201118a6d7c360d6f9e78ea80dcaf
chosaihim <chosaihim@gmail.com>
2021-06-20 00:26:15 +0900

    RGB 거리 dp로 풀기

--- codingTest/graph/1149_RGB거리.py
+++ codingTest/graph/1149_RGB거리.py
@@ -2,14 +2,17 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-graph = []
+dp = [[0]*3 for _ in range(n)]
 
+# 0th dp
+dp[0] = list(map(int, input().split()))
 
-# 3
-# 26 40 83
-# 49 60 57
-# 13 89 99
-
-# 2
-# 1 2 3
-# 4 5 6
+for node in range(1,n):
+    r,g,b = map(int,input().split())
+    
+    # R,G,B = 0,1,2
+    dp[node][0] = min(dp[node-1][1],dp[node-1][2]) + r
+    dp[node][1] = min(dp[node-1][0],dp[node-1][2]) + g
+    dp[node][2] = min(dp[node-1][0],dp[node-1][1]) + b
+    
+print(min(dp[-1]))
